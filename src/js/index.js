aed89b879b70e4260518ed402dc9be672b4a5c44
Paulo Silva <progpaulosilva@gmail.com>
2024-01-27 01:42:20 -0300

    adicionado HTTPS no link da api

--- src/js/index.js
+++ src/js/index.js
@@ -10,7 +10,7 @@ searchButton.addEventListener('click', async() => {
 })
 
 async function buscarDadosDeClimaDaCidade(city){
-    const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no&lang=pt`;
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no&lang=pt`;
     console.log(apiUrl)
 
     const response = await fetch(apiUrl);
